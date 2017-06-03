f04f40f37c4194a0c8415131f17cd1534102c997
James Kurtz <jkrrv@jkrrv.org>
2017-06-02 23:30:49 -0600

    essentially deployable

--- json/index.php
+++ json/index.php
@@ -3,21 +3,113 @@
 require_once '../vendor/autoload.php';
 
 use \jkrrv\YouTubeLiveEmbed;
+use \GuzzleHttp\Client;
+use \GuzzleHttp\HandlerStack;
+use \Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
+use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
+use Doctrine\Common\Cache\FilesystemCache;
 
 YouTubeLiveEmbed::setApiKey('AIzaSyChOgE1uQkFhxo1xOOdhlSCscmTvR2YcCk');
 
-$ytle = new YouTubeLiveEmbed('UC_GR2sUKyKPiULviFLvPDQg');
-$ytV= $ytle->videos();
-
 $r = (object)[];
-$r->live = (object)[];
-$r->live->youtube = [];
+$r->live = [];
+$r->archive = [];
 
 
-foreach($ytV as $v) {
-	$r->live->youtube[] = "//www.youtube.com/embed/" . $v->id . "?autoplay=1&rel=0&showinfo=0";
+// Create default HandlerStack, add cache to the stack.
+$stack = HandlerStack::create();
+$stack->push(
+	new CacheMiddleware(
+		new GreedyCacheStrategy(
+			new DoctrineCacheStorage(
+				new FilesystemCache('../tmp/')
+			),
+			10 // the TTL in seconds
+		)
+	),
+	'greedy-cache'
+);
+
+// YouTube Query
+$ytle = new YouTubeLiveEmbed('UC_GR2sUKyKPiULviFLvPDQg');
+$ytle->guzzleClient = new Client(['handler' => $stack]); // replace guzzle client with this one, with the handler option
+$ytV = $ytle->videos();
+
+//// YouTube: Just in case there aren't any current live streams...
+//if (count($ytV) == 0) { // test video from California Academy of Natural Sciences
+//	$ytle = new YouTubeLiveEmbed('UCZvXaNYIcapCEcaJe_2cP7A');
+//	$ytle->guzzleClient = new Client(['handler' => $stack]); // replace guzzle client with this one, with the handler option
+//	$ytV= $ytle->videos();
+//}
+
+// SermonAudio Query
+
+$sourceID = 'tenth';
+
+
+$sa_curl = file_get_contents('http://www.sermonaudio.com/playwebcast.asp?SourceID='.$sourceID);
+$sa_urlPos = strpos($sa_curl, "file:") + 7;
+$sa_urlEnd = strpos($sa_curl, "'", $sa_urlPos);
+$sa_videoUrl = substr($sa_curl, $sa_urlPos, $sa_urlEnd - $sa_urlPos);
+$sa_urlPos = strpos($sa_curl, "image:", $sa_urlEnd) + 8;
+$sa_urlEnd = strpos($sa_curl, "'", $sa_urlPos);
+$sa_imageUrl = substr($sa_curl, $sa_urlPos, $sa_urlEnd - $sa_urlPos);
+$sa = (object)[
+	'isLive' => (strpos($sa_curl, "currently not in progress") === false),
+	'videoIfrUrl' => "//www.sermonaudio.com/playwebcast.asp?sourceid=" . $sourceID . "&max=true&autoplay=true",
+	'videoUrl' => $sa_videoUrl, // this is the m3u8 file
+	'audioIfrUrl' => "//www.sermonaudio.com/playwebcast.asp?sourceid=" . $sourceID . "&max=true&autoplay=true&stream=audioonly",
+	'audioUrl' => $sa_videoUrl . "?wowzaaudioonly=true", // this is the m3u8 file
+	'thumbUrl' => $sa_imageUrl
+];
+
+
+// if live, define event.
+if (count($ytV) > 0 || $sa->isLive) {
+	$r->live[] = (object)[];
+	$r->live[0]->name = "Tenth Presbyterian Live Stream Event"; // eventually, replace with useful things from APIs.
+	$r->live[0]->priority = 1;
+	$r->live[0]->id = "current-livestream"; // replace with something unique for each event
+	$r->live[0]->sources = [];
 }
 
-$r->live->youtube[] = "//www.youtube.com/embed/eQQ9hxGbZgs?autoplay=1&rel=0&showinfo=0";
 
+// YouTube: source object
+if (count($ytV) > 0) {
+	$sources = &$r->live[0]->sources;
+	$v = $ytV[0];
+	$sources[] = (object)[
+		'type' => 'yt',
+		'language' => 'en-us',
+		'id' => "yt-" . $v->id,
+		'url' => "//www.youtube.com/embed/" . $v->id . "?autoplay=1&rel=0&showinfo=0",
+		'thumb' => $v->thumb_high
+	];
+	unset($sources);
+}
+
+
+// SermonAudio: source object
+if ($sa->isLive) {
+	$sources = &$r->live[0]->sources;
+	$sources[] = (object)[
+		'type' => 'sa-vid',
+		'language' => 'en-us',
+		'id' => "sa-vid",
+		'url' => $sa->videoIfrUrl,
+		'thumb' => $sa->thumbUrl
+	];
+	$sources[] = (object)[
+		'type' => 'sa-aud',
+		'language' => 'en-us',
+		'id' => "sa-aud",
+		'url' => $sa->audioIfrUrl,
+		'thumb' => $sa->thumbUrl
+	];
+	unset($sources);
+}
+
+
+header("Content-Type: application/json");
 echo json_encode($r);
--- script.js
+++ script.js
@@ -1,54 +1,149 @@
+// load the knockout library & separate css file
+var knockoutLib = document.createElement('script'),
+    head  = document.getElementsByTagName('head')[0],
+    stylesht = document.createElement('link');
+
+knockoutLib.src = 'node_modules/knockout/build/output/knockout-latest.js';
+knockoutLib.type = 'text/javascript';
+knockoutLib.onload = initalizeModels;
+head.appendChild(knockoutLib);
+
+stylesht.rel  = 'stylesheet';
+stylesht.type = 'text/css';
+stylesht.href = 'style.min.css';
+head.appendChild(stylesht);
+
 var scripts = document.getElementsByTagName('script'),
     container = scripts[scripts.length-1].parentNode,
-    ifr = document.createElement('iframe'),
-    loadingP = document.createElement('p'),
-    currentMode = null;
+    mediaElt = document.createElement('p'),
+    div = document.createElement('div'),
+    progList = document.createElement('div'),
+    vm = {};
 
 
 // impose some styling on the container.
 container.style.width = "100%";
 container.style.paddingBottom = "56.25%";
+container.style.marginBottom = "4em"; // space for progList
 container.style.position = "relative";
 container.style.font = "inherit";
 container.style.fontFamily = "\"Whitney SSm A\", \"Whitney SSm B\", \"Open Sans\", sans-serif";
+container.style.borderRadius = 0;
 
-// place iframe for later.
-ifr.style.width = "100%";
-ifr.style.height = "100%";
-ifr.style.position = "absolute";
-ifr.style.left = 0;
-ifr.style.right = 0;
-ifr.style.border = 0;
-ifr.style.display = "none";
-ifr.setAttribute('allowFullScreen','');
+// place div for later.
+div.style.width = "100%";
+div.style.height = "100%";
+div.style.position = "absolute";
+div.style.left = 0;
+div.style.right = 0;
+div.style.border = 0;
+div.style.display = "none";
+container.appendChild(div);
+
+// place progList for later.
+progList.id = "progList";
+progList.setAttribute("data-bind", "foreach: vm.livePrograms");
+progList.innerHTML = "<div data-bind=\"foreach: sources, visible: sources.length > 1 \"><a data-bind=\"attr: {title: providerName(type), class: type, id: id }, css: { 'active': vm.currentMode() === id }, click: playSource \"></a></div>";
+container.appendChild(progList);
 
 // insert "loading" into container.
-loadingP.style.width = "100%";
-loadingP.style.top = "45%";
-loadingP.style.position = "absolute";
-loadingP.innerHTML = "loading...";
-loadingP.style.backgroundColor = "#eee";
-container.appendChild(loadingP);
+mediaElt.style.width = "100%";
+mediaElt.style.top = "45%";
+mediaElt.style.position = "absolute";
+mediaElt.innerHTML = "loading...";
+mediaElt.style.backgroundColor = "#eee";
+container.appendChild(mediaElt);
+
+function providerName(type) {
+    switch (type) {
+        case "yt":
+            return "YouTube Video";
+        case "sa-vid":
+            return "SermonAudio Video";
+        case "sa-aud":
+            return "Audio only";
+    }
+}
+
+function initalizeModels() {
+
+    vm.livePrograms = ko.observableArray([]);
+    vm.currentMode = ko.observable("loading");
+
+    ko.applyBindings(vm, container);
+
+    doRequest();
+    setInterval(doRequest, 5000);
+}
 
 // listener for response from server
 function liveStreamJsonListener() {
-    // receive response.  Determine new mode.
-
-    // if new mode is not different from current mode, return.
-
-    // change modes as appropriate.
-
+    // receive response.
     var response = JSON.parse(this.responseText);
 
-    ifr.src = response.live.youtube[0];
-    ifr.style.display = "block";
+    vm.livePrograms(response.live);
 
-    container.appendChild(ifr);
-    loadingP.style.display = "none";
+    if (vm.currentMode() == "loading") {
+        if (response.live.length > 0) {
+            playSource(response.live[0].sources[0]);
+        } else {
+            mediaElt.innerHTML = "There is no livestream currently available.<br />We will display the livestream here as soon as it begins."
+        }
+    }
+
 }
 
-req = new XMLHttpRequest();
-req.addEventListener('load', liveStreamJsonListener);
-req.open("GET", "json");
-req.send();
+function doRequest() {
+    var req = new XMLHttpRequest();
+    req.addEventListener('load', liveStreamJsonListener);
+    req.open("GET", "json?current=" + vm.currentMode());
+    // req.open("GET", "json/test.json?current=" + vm.currentMode());
+    req.send();
+}
+
+
+function playSource(source) {
+    clearVideoWindow();
+    vm.currentMode(source.id);
+    switch (source.type) {
+        case "yt":
+            playYouTube(source);
+            return;
+        case "sa-vid":
+            playSaVid(source);
+            return;
+        case "sa-aud":
+            playSaAud(source);
+            return;
+    }
+}
+
+
+function playYouTube(source) {
+    mediaElt.src = source.url;
+}
+
+function playSaVid(source) {
+    mediaElt.src = source.url;
+}
+
+function playSaAud(source) {
+    mediaElt.src = source.url;
+}
+
+
+function clearVideoWindow() {
+    //place mediaElt for later
+    container.removeChild(mediaElt);
+    mediaElt = document.createElement('iframe');
+    mediaElt.style.width = "100%";
+    mediaElt.style.height = "100%";
+    mediaElt.style.position = "absolute";
+    mediaElt.style.left = 0;
+    mediaElt.style.right = 0;
+    mediaElt.style.border = 0;
+    mediaElt.setAttribute('allowFullScreen','');
+    container.appendChild(mediaElt);
+}
+
 
