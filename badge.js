9aa16cfe65c1df71472b8d8a62c60c403f40ae7d
James K <jkrrv@jkrrv.org>
2017-11-10 01:11:36 -0500

    Creating the Badge Feature to make some progress on #8

---
+++ badge.js
@@ -0,0 +1,5 @@
+
+var badge = document.createElement('span');
+badge.style = "font-size: 20px; display: block; margin-top: 1em; font-variant: small-caps; border: 1px solid red; padding: .3em; border-radius: .2em; color: red; font-family: \"Whitney SSm A\", \"Whitney SSm B&quot\", \"Open Sans\", open-sans, Sans-Serif; font-weight: 700;";
+badge.innerHTML = "Live Now";
+document.getElementsByClassName("fa fa-play")[0].appendChild(badge);
