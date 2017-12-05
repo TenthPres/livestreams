6161d291a640f71a3e860b67e55f05a803014dbb
James K <jkrrv@jkrrv.org>
2017-12-04 23:01:36 -0500

    Switch to using CSS to change the appearance of the main button.

--- badge/badge.js
+++ badge/badge.js
@@ -1,10 +1,5 @@
 
-var badge = document.createElement('span');
-badge.setAttribute("style", "font-size: 20px; display: block; margin-top: 1em; font-variant: small-caps; border: 1px solid red; padding: .2em; border-radius: .2em; color: red; font-family: \"Whitney SSm A\", \"Whitney SSm B\", \"Open Sans\", open-sans, Sans-Serif; font-weight: 700;");
-badge.innerHTML = "Live Now";
-document.getElementsByClassName("fa fa-play")[0].appendChild(badge);
-
-
+// Header Stuff.  If this is actually going to be used, it should be implemented more like the stuff below.
 var headerBadge = document.createElement('span'),
     headerLink = document.createElement('li');
 headerBadge.innerHTML = "Live Now";
@@ -12,3 +7,19 @@ headerBadge.setAttribute("style", "display: inline-block;font-variant: small-cap
 headerLink.innerHTML = "<a href=\"/livestream\" id=\"livestreamHeaderLink\">Livestream&nbsp;</a>&nbsp;";
 document.getElementsByClassName("navbar-nav nav navbar-right quicklinks")[0].appendChild(headerLink);
 document.getElementById('livestreamHeaderLink').appendChild(headerBadge);
+
+
+// This adds a magical class name to the body tag.  When present, the "Live Now" stuff lights up.
+document.body.classList.add('livestreamActive');
+
+
+// This just adds a style sheet.  It could probably just as well be added directly to the body.
+
+var stylesht = document.createElement('link'),
+    head  = document.getElementsByTagName('head')[0],
+    scriptBase = document.scripts[document.scripts.length-1].src.replace(/(\/\/.*?\/.*\/).*/g, '$1');
+
+stylesht.rel  = 'stylesheet';
+stylesht.type = 'text/css';
+stylesht.href = scriptBase + 'badgeActive.min.css';
+head.appendChild(stylesht);
