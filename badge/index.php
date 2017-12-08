0c54d01a75060d6090f0436cf1fb950d969210a3
James K <jkrrv@jkrrv.org>
2017-12-07 19:31:56 -0500

    Switching to use minified css and js

--- badge/index.php
+++ badge/index.php
@@ -2,4 +2,4 @@
 
 $body = file_get_contents("https://www.tenth.org/");
 
-echo str_replace("<head>", "<head><link rel=\"stylesheet\" type=\"text/css\" href=\"badge.min.css\"><script src=\"../script.js\"></script>", $body);
+echo str_replace("<head>", "<head><link rel=\"stylesheet\" type=\"text/css\" href=\"badge.min.css\"><script src=\"../script.min.js\"></script>", $body);
